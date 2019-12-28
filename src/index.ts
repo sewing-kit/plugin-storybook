579eb52cd42f34eb59b248e3c9bc7bcda01e87b7
Chris Sauve <chrismsauve@gmail.com>
2019-12-27 20:32:53 -0500

    Add initial plugin

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,369 @@
-import {createProjectDevPlugin, WaterfallHook} from '@sewing-kit/plugins';
+import * as fs from 'fs';
+import * as path from 'path';
+
+import {
+  LogLevel,
+  WaterfallHook,
+  DiagnosticError,
+  MissingPluginError,
+  createProjectDevPlugin,
+} from '@sewing-kit/plugins';
+import {} from '@sewing-kit/plugin-webpack';
 
 declare module '@sewing-kit/hooks' {
   interface DevProjectConfigurationCustomHooks {
-    readonly storybookPort: WaterfallHook<number>;
+    readonly storybookPort: WaterfallHook<number | undefined>;
+    readonly storybookOutput: WaterfallHook<string>;
+    readonly storybookEntries: WaterfallHook<string[]>;
+    readonly storybookWebpackPlugins: WaterfallHook<import('webpack').Plugin[]>;
+    readonly storybookWebpackConfig: WaterfallHook<
+      import('webpack').Configuration
+    >;
+    readonly storybookManagerWebpackConfig: WaterfallHook<
+      import('webpack').Configuration
+    >;
   }
 }
 
-export function storybook() {
-  return createProjectDevPlugin('Storybook', ({project, hooks}) => {
-    hooks.configureHooks.hook(
-      (hooks: import('@sewing-kit/hooks').DevProjectConfigurationHooks) => ({
+export interface Options {
+  port?: number;
+  config?: string;
+}
+
+const storybookCorePath = path.dirname(
+  require.resolve('@storybook/core/package.json'),
+);
+
+const dllPath = path.join(storybookCorePath, 'dll');
+
+export function storybook({port: defaultPort, config}: Options = {}) {
+  return createProjectDevPlugin(
+    'Storybook',
+    ({project, hooks, api, workspace}) => {
+      const configFile = config
+        ? project.fs.resolvePath(config)
+        : project.fs.resolvePath('.storybook/config');
+
+      hooks.configureHooks.hook((hooks: any) => ({
         ...hooks,
-        storybookPort: new WaterfallHook(),
-      }),
-    );
-  });
+        storybookPort: new WaterfallHook<number>(),
+        storybookOutput: new WaterfallHook(),
+        storybookEntries: new WaterfallHook(),
+        storybookWebpackPlugins: new WaterfallHook(),
+        storybookWebpackConfig: new WaterfallHook(),
+        storybookManagerWebpackConfig: new WaterfallHook(),
+      }));
+
+      (hooks as import('@sewing-kit/hooks').DevPackageHooks).steps.hook(
+        (steps, {configuration}) => {
+          if (configuration.webpackConfig == null) {
+            throw new MissingPluginError('@sewing-kit/plugin-webpack');
+          }
+
+          const step = api.createStep(
+            {label: 'Running storybook', indefinite: true},
+            async (step) => {
+              let webpackDevMiddlewareInstance:
+                | ReturnType<typeof webpackDevMiddleware>
+                | undefined;
+
+              const outputDir = path.join(storybookCorePath, 'dist/public');
+              const [
+                {default: getPort},
+                {default: express, Router},
+                {default: webpack, DefinePlugin, HotModuleReplacementPlugin},
+                {default: HtmlWebpackPlugin},
+                {default: TerserWebpackPlugin},
+                {default: webpackDevMiddleware},
+                {default: webpackHotMiddleware},
+                {default: webpackMerge},
+              ] = await Promise.all([
+                import('get-port'),
+                import('express'),
+                import('webpack'),
+                import('html-webpack-plugin'),
+                import('terser-webpack-plugin'),
+                import('webpack-dev-middleware'),
+                import('webpack-hot-middleware'),
+                import('webpack-merge'),
+              ] as const);
+
+              const app = express();
+              const router = Router();
+
+              const [aliases, extensions, rules] = await Promise.all([
+                configuration.webpackAliases!.run({}),
+                configuration.webpackExtensions!.run([]),
+                configuration.webpackRules!.run([]),
+              ] as const);
+
+              async function runManager() {
+                const loadManagerConfig = require('@storybook/core/dist/server/manager/manager-config')
+                  .default;
+                const managerWebpackConfig: import('webpack').Configuration = await configuration.storybookManagerWebpackConfig!.run(
+                  webpackMerge(
+                    await loadManagerConfig({
+                      configType: 'DEVELOPMENT',
+                      outputDir,
+                      configDir: path.dirname(configFile),
+                      cache: {},
+                      corePresets: [
+                        path.join(
+                          storybookCorePath,
+                          'dist/server/manager/manager-preset.js',
+                        ),
+                      ],
+                    }),
+                    {
+                      resolve: {extensions: extensions as any, alias: aliases},
+                      module: {
+                        rules: rules as any,
+                      },
+                    },
+                  ),
+                );
+
+                await new Promise((resolve) => {
+                  webpack(managerWebpackConfig).watch(
+                    {
+                      aggregateTimeout: 1,
+                      ignored: /node_modules/,
+                    },
+                    (error, stats) => {
+                      if (error || stats.hasErrors()) {
+                        if (webpackDevMiddlewareInstance) {
+                          try {
+                            webpackDevMiddlewareInstance.close();
+                            step.log('Closed Storybook preview build process', {
+                              level: LogLevel.Errors,
+                            });
+                          } catch {
+                            step.log(
+                              'Unable to close Storybook preview build process',
+                              {level: LogLevel.Errors},
+                            );
+                          }
+                        }
+
+                        throw new DiagnosticError({
+                          title: 'Storybook manager compilation failed',
+                          content: `The compilation failed with the following output:\n\n${stats.toString(
+                            'errors-warnings',
+                          )}`,
+                          suggestion:
+                            'Try reinstalling your Storybook dependencies, as this generally indicates that Storybook’s web app code has been changed in your local node_modules.',
+                        });
+                      } else {
+                        resolve(stats);
+                      }
+                    },
+                  );
+                });
+              }
+
+              async function runPreview() {
+                const [entries, plugins, outputDirectory] = await Promise.all([
+                  configuration.storybookEntries!.run([
+                    path.join(
+                      storybookCorePath,
+                      'dist/server/common/polyfills.js',
+                    ),
+                    path.join(
+                      storybookCorePath,
+                      'dist/server/preview/globals.js',
+                    ),
+                    configFile,
+                    `${require.resolve(
+                      'webpack-hot-middleware/client.js',
+                    )}?reload=true&quiet=true`,
+                  ]),
+                  configuration.storybookWebpackPlugins!.run([
+                    new HtmlWebpackPlugin({
+                      filename: 'iframe.html',
+                      chunksSortMode: 'none',
+                      alwaysWriteToDisk: true,
+                      inject: false,
+                      templateParameters: (compilation, files, options) => ({
+                        compilation,
+                        files,
+                        options,
+                        version: require('@storybook/core/package.json')
+                          .version,
+                        globals: {},
+                        headHtmlSnippet: getPreviewHeadHtml(process.env),
+                        dlls: [],
+                        bodyHtmlSnippet: getPreviewBodyHtml(process.env),
+                      }),
+                      template: path.join(
+                        storybookCorePath,
+                        'dist/server/templates/index.ejs',
+                      ),
+                    }),
+                    new DefinePlugin({
+                      NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+                      'process.env': {
+                        NODE_ENV: '"development"',
+                        NODE_PATH: '""',
+                        PUBLIC_URL: '"."',
+                      },
+                    }),
+                    new HotModuleReplacementPlugin(),
+                  ]),
+                  configuration.storybookOutput!.run(
+                    workspace.fs.buildPath('storybook'),
+                  ),
+                ] as const);
+
+                const previewWebpackConfig = await configuration.webpackConfig!.run(
+                  {
+                    mode: 'development',
+                    entry: entries,
+                    output: {
+                      publicPath: '',
+                      filename: '[name].[hash].bundle.js',
+                      path: outputDirectory,
+                    },
+                    devtool: '#cheap-module-source-map',
+                    plugins: plugins as any,
+                    module: {rules: rules as any},
+                    resolve: {
+                      extensions: extensions as any,
+                      alias: aliases,
+                    },
+                    optimization: {
+                      splitChunks: {
+                        chunks: 'all',
+                      },
+                      runtimeChunk: true,
+                      minimizer: [
+                        new TerserWebpackPlugin({
+                          cache: true,
+                          parallel: true,
+                          sourceMap: true,
+                          terserOptions: {
+                            mangle: false,
+                            // eslint-disable-next-line @typescript-eslint/camelcase
+                            keep_fnames: true,
+                          },
+                        }),
+                      ],
+                    },
+                  },
+                );
+
+                const previewCompiler = webpack(
+                  await configuration.storybookWebpackConfig!.run(
+                    previewWebpackConfig,
+                  ),
+                );
+                webpackDevMiddlewareInstance = webpackDevMiddleware(
+                  previewCompiler,
+                  {
+                    publicPath: previewWebpackConfig.output!.publicPath!,
+                    watchOptions: {
+                      aggregateTimeout: 1,
+                      ignored: /node_modules/,
+                    },
+                    // this actually causes 0 (regular) output from wdm & webpack
+                    logLevel: 'warn',
+                  },
+                );
+
+                router.use(webpackDevMiddlewareInstance);
+                router.use(webpackHotMiddleware(previewCompiler));
+
+                await new Promise((resolve, reject) => {
+                  webpackDevMiddlewareInstance!.waitUntilValid((stats) => {
+                    if (!stats) {
+                      reject(
+                        new DiagnosticError({
+                          title: 'Unable to build preview',
+                        }),
+                      );
+                    } else if (stats.hasErrors()) {
+                      reject(
+                        new DiagnosticError({
+                          title: 'Unable to build preview',
+                          content: `The preview failed to build with the following output:\n\n${stats.toString(
+                            'errors-warnings',
+                          )}`,
+                        }),
+                      );
+                    } else {
+                      resolve(stats);
+                    }
+                  });
+                });
+              }
+
+              await Promise.all([runManager(), runPreview()]);
+
+              router.get('/', (_, response) => {
+                response.set('Content-Type', 'text/html');
+                response.sendFile(path.join(`${outputDir}/index.html`));
+              });
+
+              router.get(/\/sb_dll\/(.+\.js)$/, (request, response) => {
+                response.set('Content-Type', 'text/javascript');
+                response.sendFile(path.join(`${dllPath}/${request.params[0]}`));
+              });
+
+              router.get(/\/sb_dll\/(.+\.LICENCE)$/, (request, response) => {
+                response.set('Content-Type', 'text/html');
+                response.sendFile(path.join(`${dllPath}/${request.params[0]}`));
+              });
+
+              router.get(/(.+\.js)$/, (request, response) => {
+                response.set('Content-Type', 'text/javascript');
+                response.sendFile(
+                  path.join(`${outputDir}/${request.params[0]}`),
+                );
+              });
+
+              app.use(router);
+
+              const port =
+                (await configuration.storybookPort!.run(defaultPort)) ??
+                (await getPort());
+              app.listen(port, () => {
+                step.log(`Running storybook on localhost:${port}`);
+              });
+            },
+          );
+
+          return [...steps, step];
+        },
+      );
+    },
+  );
+}
+
+const interpolate = (string: string, data = {}) =>
+  Object.entries(data).reduce(
+    (acc, [k, v]) => acc.replace(new RegExp(`%${k}%`, 'g'), v as any),
+    string,
+  );
+
+function getPreviewBodyHtml(interpolations: object) {
+  const template = fs.readFileSync(
+    path.join(
+      storybookCorePath,
+      'dist/server/templates/base-preview-body.html',
+    ),
+    'utf8',
+  );
+
+  return interpolate(template, interpolations);
+}
+
+function getPreviewHeadHtml(interpolations: object) {
+  const template = fs.readFileSync(
+    path.join(
+      storybookCorePath,
+      'dist/server/templates/base-preview-head.html',
+    ),
+    'utf8',
+  );
+
+  return interpolate(template, interpolations);
 }
